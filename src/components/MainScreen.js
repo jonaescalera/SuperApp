44bf3e266ae29cc9b7ad6cac4112dae1b5952e5c
jonaescalera <jonaescalera@gmail.com>
2021-05-21 16:06:02 -0300

    First commit about superApp face recognition

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,51 +1,47 @@
 import React from 'react';
 import {View, Pressable, StyleSheet, Text} from 'react-native';
 
-class MainScreen extends React.Component{
+class MainScreen extends React.Component {
+  handlePressPicture = () => {
+    console.log('Go to picture ');
+    this.props.navigation.navigate('PictureScreen');
+  };
 
-    handlePressPicture = () =>{
-        console.log('Go to picture ');
-        this.props.navigation.navigate('PictureScreen');
-    }
-    
-    handlePressVideo = () =>{
-        console.log('Go to video ');
-        this.props.navigation.navigate('VideoScreen');
-    }
+  handlePressVideo = () => {
+    console.log('Go to video ');
+    this.props.navigation.navigate('VideoScreen');
+  };
 
-    render(){
-        return(
-            <View style={styles.mainMenu}>
-                <Pressable onPress={this.handlePressPicture}
-                style={styles.btn}>
-                   <Text style={styles.txt}>Fotografia</Text>
-                </Pressable>
-                <Pressable onPress={this.handlePressVideo}
-                style={styles.btn}>
-                   <Text style={styles.txt}>Video</Text>
-                </Pressable>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.mainMenu}>
+        <Pressable onPress={this.handlePressPicture} style={styles.btn}>
+          <Text style={styles.txt}>Foto</Text>
+        </Pressable>
+        <Pressable onPress={this.handlePressVideo} style={styles.btn}>
+          <Text style={styles.txt}>Video</Text>
+        </Pressable>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    mainMenu:{
-        backgroundColor: '#050',
-        flex: 1,
-    },
-    btn:{
-        backgroundColor: '#025',
-        padding: 0,
-        borderRadius: 8,
-        margin: 16,
-    },
-    txt:{
-        color: '#ff0',
-        textAlign: 'center',
-        fontSize: 20,
-    },
+  mainMenu: {
+    backgroundColor: '#050',
+    flex: 1,
+  },
+  btn: {
+    backgroundColor: '#025',
+    padding: 0,
+    borderRadius: 8,
+    margin: 16,
+  },
+  txt: {
+    color: '#ff0',
+    textAlign: 'center',
+    fontSize: 20,
+  },
 });
 
 export default MainScreen;
-
--- src/components/PictureScreen.js
+++ src/components/PictureScreen.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import {RNCamera} from 'react-native-camera';
 import PendingView from './PendingView';
 
-
-class PictureScreen extends React.Component{
-
+class PictureScreen extends React.Component {
   state = {
     pausePreview: false,
-  }
+    fd: true,
+    faceData: [],
+  };
 
-  render(){
+  render() {
     const {pausePreview} = this.state;
     return (
       <View style={styles.container}>
         <RNCamera
           style={styles.preview}
-          type={RNCamera.Constants.Type.back}
+          type={RNCamera.Constants.Type.front}
           flashMode={RNCamera.Constants.FlashMode.off}
+          faceDetectionClassifications={
+            RNCamera.Constants.FaceDetection.Classifications.all
+          }
+          faceDetectionLandmarks={
+            RNCamera.Constants.FaceDetection.Landmarks.all
+          }
+          //faceDetectionMode={RNCamera.Constants.FaceDetection.Mode.fast}
+          onFacesDetected={(face) => {
+            if (this.state.fd) {
+              this.setState({fd: face.faces.length === 0});
+              this.setState({faceData: JSON.stringify(face)});
+              //alert(JSON.stringify(face));
+            }
+          }}
           androidCameraPermissionOptions={{
             title: 'Permission to use camera',
             message: 'We need your permission to use your camera',
@@ -29,22 +43,28 @@ class PictureScreen extends React.Component{
             message: 'We need your permission to use your audio',
             buttonPositive: 'Ok',
             buttonNegative: 'Cancel',
-          }}
-        >
-          {({ camera, status, recordAudioPermissionStatus }) => {
+          }}>
+          {({camera, status, recordAudioPermissionStatus}) => {
             if (status !== 'READY') return <PendingView />;
             return (
-              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                {pausePreview?
-                <TouchableOpacity style={styles.button}
-                onPress={() => this.resumePicture(camera)}>
-                  <Text>Aceptar</Text>
-                </TouchableOpacity>
-                :null
-                }
+              <View
+                style={{
+                  flex: 0,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                }}>
+                {pausePreview ? (
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => this.resumePicture(camera)}>
+                    <Text>Aceptar</Text>
+                  </TouchableOpacity>
+                ) : null}
 
-                <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.button}>
-                  <Text style={{ fontSize: 14 }}> SNAP </Text>
+                <TouchableOpacity
+                  onPress={() => this.takePicture(camera)}
+                  style={styles.button}>
+                  <Text style={{fontSize: 14}}> SNAP </Text>
                 </TouchableOpacity>
               </View>
             );
@@ -53,24 +73,25 @@ class PictureScreen extends React.Component{
       </View>
     );
   }
-  takePicture = async function(camera) {
-    const options = { quality: 0.5, base64: true };
+  takePicture = async function (camera) {
+    const options = {quality: 0.5, base64: true, exif: false};
     const data = await camera.takePictureAsync(options);
     //  eslint-disable-next-line
     const source = data.uri;
+    const faceValues = this.state.faceData;
     if (source) {
       await camera.pausePreview();
-      console.log("picture source", source);
-      this.setState({ pausePreview:true })
+      console.log('picture source', faceValues);
+      console.log('Dato en Base 64', data.base64);
+      this.setState({pausePreview: true});
     }
   };
-    
-  resumePicture = async function(camera){
-    await camera.resumePreview()    
-    this.setState({pausePreview:false})
-  }
-}
 
+  resumePicture = async function (camera) {
+    await camera.resumePreview();
+    this.setState({pausePreview: false});
+  };
+}
 
 const styles = StyleSheet.create({
   container: {
